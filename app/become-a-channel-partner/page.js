710bf39613f6277e498b5f1fd555da49e1983b85
Mobashir Alam <mobashira3@gmail.com>
2025-05-05 02:29:24 +0530

    fixed channel partner form

--- app/become-a-channel-partner/page.js
+++ app/become-a-channel-partner/page.js
@@ -5,9 +5,10 @@ const ChannelPartner = () => {
     const title = "Become a Channel Partner"
     const content = "Join 4SFS as a Channel Partner and grow your business with India's leading real estate investment platform."
     const ctaText = "Join Now"
+    const formTitle = "Become a Channel Partner"
   return (
     <div>
-        <PartnerHomepage title={title} content={content} ctaText={ctaText} />
+        <PartnerHomepage title={title} content={content} ctaText={ctaText} formTitle={formTitle} />
     </div>
   )
 }
